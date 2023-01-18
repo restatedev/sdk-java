7552646d46a58e3ace6ea25f37b27a2aed40a20c
slinkydeveloper <francescoguard@gmail.com>
2023-01-18 12:37:19 +0100

    DeferredResultInternal interface and SingleDeferredResult implementation.
    SingleDeferredResult is mutable, to allow querying the same deferred without going through syscalls. This is also relevant for the upcoming implementation of any and all.

---
+++ sdk-core-impl/src/main/java/dev/restate/sdk/core/impl/SingleDeferredResult.java
@@ -0,0 +1,41 @@
+package dev.restate.sdk.core.impl;
+
+import dev.restate.sdk.core.syscalls.ReadyResult;
+import java.util.Map;
+import javax.annotation.Nullable;
+
+class SingleDeferredResult<T> implements DeferredResultInternal<T> {
+
+  private final int entryIndex;
+  private ReadyResultInternal<T> inner;
+
+  SingleDeferredResult(int entryIndex) {
+    this.entryIndex = entryIndex;
+  }
+
+  @Override
+  public boolean isCompleted() {
+    return inner != null;
+  }
+
+  @Override
+  public int entryIndex() {
+    return entryIndex;
+  }
+
+  @Override
+  @Nullable
+  public ReadyResult<T> toReadyResult() {
+    return inner;
+  }
+
+  @SuppressWarnings("unchecked")
+  public boolean tryResolve(Map<Integer, ReadyResultInternal<?>> resultMap) {
+    ReadyResultInternal<?> resolved = resultMap.remove(entryIndex);
+    if (resolved != null) {
+      inner = (ReadyResultInternal<T>) resolved;
+      return true;
+    }
+    return false;
+  }
+}
