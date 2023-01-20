73f3fe8abf19b0a7e56e05d509194b1bd8c2c2c0
slinkydeveloper <francescoguard@gmail.com>
2023-01-20 12:43:09 +0100

    Implement factory methods for any and all

--- sdk-core-impl/src/main/java/dev/restate/sdk/core/impl/SyscallsInternal.java
+++ sdk-core-impl/src/main/java/dev/restate/sdk/core/impl/SyscallsInternal.java
@@ -1,8 +1,24 @@
 package dev.restate.sdk.core.impl;
 
+import dev.restate.sdk.core.impl.DeferredResults.DeferredResultInternal;
+import dev.restate.sdk.core.syscalls.DeferredResult;
 import dev.restate.sdk.core.syscalls.Syscalls;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public interface SyscallsInternal extends Syscalls {
+
+  @Override
+  default DeferredResult<Object> createAnyDeferred(Collection<DeferredResult<?>> children) {
+    return DeferredResults.any(
+        children.stream().map(dr -> (DeferredResultInternal<?>) dr).collect(Collectors.toList()));
+  }
+
+  @Override
+  default DeferredResult<Void> createAllDeferred(Collection<DeferredResult<?>> children) {
+    return DeferredResults.all(
+        children.stream().map(dr -> (DeferredResultInternal<?>) dr).collect(Collectors.toList()));
+  }
 
   // -- Lifecycle methods
 
