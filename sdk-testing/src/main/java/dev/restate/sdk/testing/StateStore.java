9c113779fd292905bffde49744ffb6da7716fb68
Giselle van Dongen <giselle.vd@gmail.com>
2023-03-02 08:55:11 +0100

    TestRestateRuntime to locally test applications end-to-end  (#55)
    Test harness to locally test applications end-to-end by using a TestRestateRuntime that:
    
    Supports multiple services
    Supports keyed/unkeyed/singleton services
    Supports get/set/clear state
    Supports inter-service calls and backgroundCalls
    Supports side effects (although they are ignored)
    Supports awakeable, awaitable.any, awaitable.all
    
    Sleeps are ignored. 
    Only supports single thread threading model.

---
+++ sdk-testing/src/main/java/dev/restate/sdk/testing/StateStore.java
@@ -0,0 +1,54 @@
+package dev.restate.sdk.testing;
+
+import com.google.protobuf.ByteString;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+class StateStore {
+
+  private static final Logger LOG = LogManager.getLogger(StateStore.class);
+
+  private final HashMap<String, ByteString> state;
+
+  private StateStore() {
+    this.state = new HashMap<>();
+  }
+
+  public static StateStore init() {
+    return new StateStore();
+  }
+
+  public void close() {
+    LOG.debug("Closing state store instance");
+    state.clear();
+  }
+
+  public ByteString get(String serviceName, String instanceKey, ByteString key) {
+    return state.get(asKey(serviceName, instanceKey, key));
+  }
+
+  public void set(String serviceName, String instanceKey, ByteString key, ByteString value) {
+    state.put(asKey(serviceName, instanceKey, key), value);
+    LOG.debug("State store contents: " + toString());
+  }
+
+  // Clears state for a single key
+  public void clear(String serviceName, String instanceKey, ByteString key) {
+    state.remove(asKey(serviceName, instanceKey, key));
+  }
+
+  private String asKey(String serviceName, String instanceKey, ByteString key) {
+    return serviceName + "/" + instanceKey + "/" + key.toStringUtf8();
+  }
+
+  @Override
+  public String toString() {
+    return state.entrySet().stream()
+        .map(
+            entry ->
+                "{ key: " + entry.getKey() + ", value: " + entry.getValue().toStringUtf8() + "}")
+        .collect(Collectors.joining(", "));
+  }
+}
