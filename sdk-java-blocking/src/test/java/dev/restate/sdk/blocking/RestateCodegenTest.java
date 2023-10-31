0a3fbc4097e9cd958447b89e4996ba3f732d08d0
slinkydeveloper <francescoguard@gmail.com>
2023-10-31 09:52:28 +0100

    Add oneWay and delayed variants for generated client methods

--- sdk-java-blocking/src/test/java/dev/restate/sdk/blocking/RestateCodegenTest.java
+++ sdk-java-blocking/src/test/java/dev/restate/sdk/blocking/RestateCodegenTest.java
@@ -2,8 +2,12 @@ package dev.restate.sdk.blocking;
 
 import static dev.restate.sdk.core.impl.ProtoUtils.*;
 import static dev.restate.sdk.core.impl.TestDefinitions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.type;
 
+import dev.restate.generated.service.protocol.Protocol;
 import dev.restate.sdk.core.impl.testservices.*;
+import java.time.Duration;
 import java.util.stream.Stream;
 
 public class RestateCodegenTest implements TestSuite {
@@ -14,6 +18,8 @@ public class RestateCodegenTest implements TestSuite {
     @Override
     public GreetingResponse greet(RestateContext context, GreetingRequest request) {
       GreeterRestate.GreeterRestateClient client = GreeterRestate.newClient();
+      client.greetDelayed(request, Duration.ofSeconds(1));
+      client.greetOneWay(request);
       return client.greet(request).await();
     }
   }
@@ -25,10 +31,38 @@ public class RestateCodegenTest implements TestSuite {
             .withInput(
                 startMessage(1),
                 inputMessage(greetingRequest("Francesco")),
-                completionMessage(1, greetingResponse("Till")))
+                completionMessage(3, greetingResponse("Till")))
             .onlyUnbuffered()
-            .expectingOutput(
-                invokeMessage(GreeterGrpc.getGreetMethod(), greetingRequest("Francesco")),
-                outputMessage(greetingResponse("Till"))));
+            .assertingOutput(
+                msgs -> {
+                  assertThat(msgs)
+                      .element(0)
+                      .asInstanceOf(type(Protocol.BackgroundInvokeEntryMessage.class))
+                      .satisfies(
+                          backgroundInvokeEntryMessage -> {
+                            // Check invoke time is non zero
+                            assertThat(backgroundInvokeEntryMessage.getInvokeTime())
+                                .isGreaterThan(0);
+                            // Check background invoke header
+                            assertThat(
+                                    backgroundInvokeEntryMessage.toBuilder()
+                                        .clearInvokeTime()
+                                        .build())
+                                .isEqualTo(
+                                    backgroundInvokeMessage(
+                                            GreeterGrpc.getGreetMethod(),
+                                            greetingRequest("Francesco"))
+                                        .build());
+                          });
+                  assertThat(msgs)
+                      .elements(1, 2, 3)
+                      .containsExactly(
+                          backgroundInvokeMessage(
+                                  GreeterGrpc.getGreetMethod(), greetingRequest("Francesco"))
+                              .build(),
+                          invokeMessage(GreeterGrpc.getGreetMethod(), greetingRequest("Francesco"))
+                              .build(),
+                          outputMessage(greetingResponse("Till")));
+                }));
   }
 }
