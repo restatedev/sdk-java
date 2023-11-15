98836c62c5cad1f0ca97e79e55d05102dd0b2080
slinkydeveloper <francescoguard@gmail.com>
2023-11-15 17:08:25 +0100

    Use TerminalException in blocking interface and related protoc gen

--- sdk-java-blocking/src/main/java/dev/restate/sdk/blocking/RestateBlockingService.java
+++ sdk-java-blocking/src/main/java/dev/restate/sdk/blocking/RestateBlockingService.java
@@ -6,16 +6,17 @@ import dev.restate.sdk.core.syscalls.Syscalls;
 /**
  * Marker interface for Restate blocking services.
  *
+ * <p>
+ *
  * <h2>Error handling</h2>
  *
  * The error handling of Restate services works as follows:
  *
  * <ul>
- *   <li>When throwing {@link io.grpc.StatusException} or {@link io.grpc.StatusRuntimeException},
- *       the failure is considered "terminal" and will be used as invocation output
+ *   <li>When throwing {@link dev.restate.sdk.core.TerminalException}, the failure will be used as
+ *       invocation response error value
  *   <li>When throwing any other type of exception, the failure is considered "non-terminal" and the
  *       runtime will retry it, according to its configuration
- *   <li>In case {@code StreamObserver#onError} is invoked, the failure is considered "terminal"
  * </ul>
  */
 public interface RestateBlockingService extends BindableBlockingService {
