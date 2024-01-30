63f3241af98644855fad2fc45e720dea0f51b8f4
slinkydeveloper <francescoguard@gmail.com>
2024-01-30 15:24:17 +0100

    Add a bunch of tests to check failure cases of CoreSerdes

--- sdk-common/src/test/java/dev/restate/sdk/common/CoreSerdesTest.java
+++ sdk-common/src/test/java/dev/restate/sdk/common/CoreSerdesTest.java
@@ -9,7 +9,9 @@
 package dev.restate.sdk.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -18,44 +20,62 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class CoreSerdesTest {
 
-  private static <T> Arguments testCase(Serde<T> serde, T value) {
+  private static <T> Arguments roundtripCase(Serde<T> serde, T value) {
     return Arguments.of(
         (value != null ? value.getClass().getSimpleName() : "Null") + ": " + value, serde, value);
   }
 
-  private static Stream<Arguments> roundtripTestCases() {
+  private static Stream<Arguments> roundtrip() {
     var random = new Random();
     return Stream.of(
-        testCase(CoreSerdes.VOID, null),
-        testCase(CoreSerdes.BYTES, new byte[] {1, 2, 3, 4}),
-        testCase(CoreSerdes.STRING_UTF8, ""),
-        testCase(CoreSerdes.STRING_UTF8, "Francesco1234"),
-        testCase(CoreSerdes.STRING_UTF8, "😀"),
-        testCase(CoreSerdes.BOOLEAN, true),
-        testCase(CoreSerdes.BOOLEAN, false),
-        testCase(CoreSerdes.BYTE, Byte.MIN_VALUE),
-        testCase(CoreSerdes.BYTE, Byte.MAX_VALUE),
-        testCase(CoreSerdes.BYTE, (byte) random.nextInt()),
-        testCase(CoreSerdes.SHORT, Short.MIN_VALUE),
-        testCase(CoreSerdes.SHORT, Short.MAX_VALUE),
-        testCase(CoreSerdes.SHORT, (short) random.nextInt()),
-        testCase(CoreSerdes.INT, Integer.MIN_VALUE),
-        testCase(CoreSerdes.INT, Integer.MAX_VALUE),
-        testCase(CoreSerdes.INT, random.nextInt()),
-        testCase(CoreSerdes.LONG, Long.MIN_VALUE),
-        testCase(CoreSerdes.LONG, Long.MAX_VALUE),
-        testCase(CoreSerdes.LONG, random.nextLong()),
-        testCase(CoreSerdes.FLOAT, Float.MIN_VALUE),
-        testCase(CoreSerdes.FLOAT, Float.MAX_VALUE),
-        testCase(CoreSerdes.FLOAT, random.nextFloat()),
-        testCase(CoreSerdes.DOUBLE, Double.MIN_VALUE),
-        testCase(CoreSerdes.DOUBLE, Double.MAX_VALUE),
-        testCase(CoreSerdes.DOUBLE, random.nextDouble()));
+        roundtripCase(CoreSerdes.VOID, null),
+        roundtripCase(CoreSerdes.BYTES, new byte[] {1, 2, 3, 4}),
+        roundtripCase(CoreSerdes.JSON_STRING, ""),
+        roundtripCase(CoreSerdes.JSON_STRING, "Francesco1234"),
+        roundtripCase(CoreSerdes.JSON_STRING, "😀"),
+        roundtripCase(CoreSerdes.JSON_BOOLEAN, true),
+        roundtripCase(CoreSerdes.JSON_BOOLEAN, false),
+        roundtripCase(CoreSerdes.JSON_BYTE, Byte.MIN_VALUE),
+        roundtripCase(CoreSerdes.JSON_BYTE, Byte.MAX_VALUE),
+        roundtripCase(CoreSerdes.JSON_BYTE, (byte) random.nextInt()),
+        roundtripCase(CoreSerdes.JSON_SHORT, Short.MIN_VALUE),
+        roundtripCase(CoreSerdes.JSON_SHORT, Short.MAX_VALUE),
+        roundtripCase(CoreSerdes.JSON_SHORT, (short) random.nextInt()),
+        roundtripCase(CoreSerdes.JSON_INT, Integer.MIN_VALUE),
+        roundtripCase(CoreSerdes.JSON_INT, Integer.MAX_VALUE),
+        roundtripCase(CoreSerdes.JSON_INT, random.nextInt()),
+        roundtripCase(CoreSerdes.JSON_LONG, Long.MIN_VALUE),
+        roundtripCase(CoreSerdes.JSON_LONG, Long.MAX_VALUE),
+        roundtripCase(CoreSerdes.JSON_LONG, random.nextLong()),
+        roundtripCase(CoreSerdes.JSON_FLOAT, Float.MIN_VALUE),
+        roundtripCase(CoreSerdes.JSON_FLOAT, Float.MAX_VALUE),
+        roundtripCase(CoreSerdes.JSON_FLOAT, random.nextFloat()),
+        roundtripCase(CoreSerdes.JSON_DOUBLE, Double.MIN_VALUE),
+        roundtripCase(CoreSerdes.JSON_DOUBLE, Double.MAX_VALUE),
+        roundtripCase(CoreSerdes.JSON_DOUBLE, random.nextDouble()));
   }
 
   @ParameterizedTest(name = "{0}")
-  @MethodSource("roundtripTestCases")
+  @MethodSource
   <T> void roundtrip(String testName, Serde<T> serde, T value) throws Throwable {
     assertThat(serde.deserialize(serde.serialize(value))).isEqualTo(value);
+  }
+
+  private static Stream<Arguments> failDeserialization() {
+    return Stream.of(
+        Arguments.of("String unquoted", CoreSerdes.JSON_STRING, "my string"),
+        Arguments.of("Not a boolean", CoreSerdes.JSON_BOOLEAN, "something"),
+        Arguments.of("Not a byte", CoreSerdes.JSON_BYTE, "something"),
+        Arguments.of("Not a short", CoreSerdes.JSON_SHORT, "something"),
+        Arguments.of("Not a int", CoreSerdes.JSON_INT, "something"),
+        Arguments.of("Not a long", CoreSerdes.JSON_LONG, "something"),
+        Arguments.of("Not a float", CoreSerdes.JSON_FLOAT, "something"),
+        Arguments.of("Not a double", CoreSerdes.JSON_DOUBLE, "something"));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource
+  <T> void failDeserialization(String testName, Serde<T> serde, String value) throws Throwable {
+    assertThatThrownBy(() -> serde.deserialize(value.getBytes(StandardCharsets.UTF_8))).isNotNull();
   }
 }
