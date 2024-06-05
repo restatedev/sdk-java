a8549e6dddde33acf6b7fac018ff9310797f3e9f
slinkydeveloper <francescoguard@gmail.com>
2024-06-05 10:32:51 +0200

    Managed to get the aggregated javadocs

--- sdk-aggregated-javadocs/build.gradle.kts
+++ sdk-aggregated-javadocs/build.gradle.kts
@@ -1,11 +1,17 @@
-plugins {
-  id("io.freefair.aggregate-javadoc") version "8.6"
-}
+plugins { id("io.freefair.aggregate-javadoc") version "8.6" }
 
-// dependencies {
-//    rootProject.subprojects { subproject ->
-//        subproject.plugins.withId("java") {
-//            javadoc subproject
-//        }
-//    }
-// }
+rootProject.subprojects
+    .filter { it.name != "examples" }
+    .forEach {
+      it.plugins.withId("java") {
+        // Add dependency
+        dependencies.javadoc(it)
+
+        tasks.javadoc { classpath += it.configurations["compileClasspath"] }
+      }
+    }
+
+tasks.javadoc {
+  title = "Restate SDK-Java documentation"
+  options.windowTitle = "Restate SDK-Java documentation"
+}
