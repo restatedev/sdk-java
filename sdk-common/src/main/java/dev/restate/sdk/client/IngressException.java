b46e2e9ffb1db540acb3072607e2eb9e5e5a71b0
slinkydeveloper <francescoguard@gmail.com>
2024-08-06 16:54:12 +0200

    More info on ingress logs

--- sdk-common/src/main/java/dev/restate/sdk/client/IngressException.java
+++ sdk-common/src/main/java/dev/restate/sdk/client/IngressException.java
@@ -8,26 +8,66 @@
 // https://github.com/restatedev/sdk-java/blob/main/LICENSE
 package dev.restate.sdk.client;
 
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import org.jspecify.annotations.Nullable;
 
 public class IngressException extends RuntimeException {
 
+  private final String requestMethod;
+  private final String requestURI;
   private final int statusCode;
   private final byte[] responseBody;
 
-  public IngressException(String message, Throwable cause) {
-    this(message, -1, null, cause);
+  public IngressException(
+      String message, String requestMethod, String requestURI, Throwable cause) {
+    this(message, requestMethod, requestURI, -1, null, cause);
   }
 
-  public IngressException(String message, int statusCode, byte[] responseBody) {
-    this(message, statusCode, responseBody, null);
+  IngressException(String message, HttpRequest request, Throwable cause) {
+    this(message, request.method(), request.uri().toString(), -1, null, cause);
   }
 
-  public IngressException(String message, int statusCode, byte[] responseBody, Throwable cause) {
+  IngressException(String message, HttpRequest request) {
+    this(message, request, null);
+  }
+
+  public IngressException(
+      String message,
+      String requestMethod,
+      String requestURI,
+      int statusCode,
+      byte[] responseBody) {
+    this(message, requestMethod, requestURI, statusCode, responseBody, null);
+  }
+
+  IngressException(String message, HttpResponse<byte[]> response, Throwable cause) {
+    this(
+        message,
+        response.request().method(),
+        response.request().uri().toString(),
+        response.statusCode(),
+        response.body(),
+        cause);
+  }
+
+  IngressException(String message, HttpResponse<byte[]> response) {
+    this(message, response, null);
+  }
+
+  public IngressException(
+      String message,
+      String requestMethod,
+      String requestURI,
+      int statusCode,
+      byte[] responseBody,
+      Throwable cause) {
     super(message, cause);
     this.statusCode = statusCode;
     this.responseBody = responseBody;
+    this.requestMethod = requestMethod;
+    this.requestURI = requestURI;
   }
 
   public int getStatusCode() {
@@ -38,9 +78,21 @@ public class IngressException extends RuntimeException {
     return responseBody;
   }
 
+  public String getRequestMethod() {
+    return requestMethod;
+  }
+
+  public String getRequestURI() {
+    return requestURI;
+  }
+
   @Override
   public String getMessage() {
     return "["
+        + requestMethod
+        + " "
+        + requestURI
+        + "][Status: "
         + statusCode
         + "] "
         + super.getMessage()
