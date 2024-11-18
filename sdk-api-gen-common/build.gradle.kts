df1115a3306333b18f977f021cfb444c8803d013
slinkydeveloper <francescoguard@gmail.com>
2024-11-18 17:27:18 +0100

    Fix code generation warning

--- sdk-api-gen-common/build.gradle.kts
+++ sdk-api-gen-common/build.gradle.kts
@@ -11,4 +11,7 @@ dependencies {
 
   api("com.github.jknack:handlebars:4.3.1")
   api(project(":sdk-common"))
+
+  // We need it to silence the slf4j warning (coming from handlebars)
+  runtimeOnly("org.slf4j:slf4j-nop:1.7.32")
 }
--- sdk-api-gen/build.gradle.kts
+++ sdk-api-gen/build.gradle.kts
@@ -11,7 +11,6 @@ dependencies {
   compileOnly(coreLibs.jspecify)
 
   implementation(project(":sdk-api-gen-common"))
-
   implementation(project(":sdk-api"))
 
   testAnnotationProcessor(project(":sdk-api-gen"))
