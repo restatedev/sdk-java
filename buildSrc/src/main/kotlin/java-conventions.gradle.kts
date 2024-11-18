0d931817ad4df4a47742c040a46f89471b2f281f
slinkydeveloper <francescoguard@gmail.com>
2024-11-18 10:22:31 +0100

    Finish bumping + remove errorprone

--- buildSrc/src/main/kotlin/java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/java-conventions.gradle.kts
@@ -1,13 +1,7 @@
-
-import net.ltgt.gradle.errorprone.errorprone
-
 plugins {
     java
-    id("net.ltgt.errorprone")
     id("com.diffplug.spotless")
 }
-
-dependencies { errorprone("com.google.errorprone:error_prone_core:2.28.0") }
 
 // Configure the java toolchain to use. If not found, it will be downloaded automatically
 java {
@@ -15,19 +9,6 @@ java {
 
     withJavadocJar()
     withSourcesJar()
-}
-
-tasks.withType<JavaCompile>().configureEach {
-    options.errorprone.disableWarningsInGeneratedCode.set(true)
-    options.errorprone.disable(
-        // We use toString() in proto messages for debugging reasons.
-        "LiteProtoToString",
-        // This check is proposing to use a guava API instead...
-        "StringSplitter",
-        // This is conflicting with a javadoc warn lint
-        "MissingSummary"
-    )
-    options.errorprone.excludedPaths.set(".*/build/generated/.*")
 }
 
 tasks.withType<Test> { useJUnitPlatform() }
