2752569e2a735f603aa62cf3c42d87d64ca7e1e0
slinkydeveloper <francescoguard@gmail.com>
2025-02-10 14:43:14 +0100

    WIP

--- serde/src/main/java/dev/restate/sdk/definition/ServiceDefinitionAndOptions.java
+++
@@ -1,7 +0,0 @@
-package dev.restate.sdk.definition;
-
-/**
- * Container of {@link ServiceDefinition} and its options.
- */
-public record ServiceDefinitionAndOptions<O>(ServiceDefinition<O> service, O options) {
-}
--- serde/src/main/java/dev/restate/sdk/types/TimeoutException.java
+++
@@ -1,8 +0,0 @@
-package dev.restate.sdk.types;
-
-public class TimeoutException extends TerminalException {
-
-    public TimeoutException(String message) {
-        super(409, message);
-    }
-}
