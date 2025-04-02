c8db8e8a67951c4686a4993db51cdfde845b9546
Francesco Guardiani <me@slinkydeveloper.com>
2025-04-02 12:28:56 +0200

    Improve spring integration deps (#484)
    * Make dependencies of `sdk-spring-boot` minimal, and make sure all the deps used by the starter modules are what people need to get started.

--- sdk-spring-boot-kotlin-starter/build.gradle.kts
+++ sdk-spring-boot-kotlin-starter/build.gradle.kts
@@ -10,16 +10,21 @@ description = "Restate SDK Spring Boot Kotlin starter"
 dependencies {
   compileOnly(libs.jspecify)
 
+  // SDK dependencies
   api(project(":sdk-api-kotlin"))
   api(project(":client-kotlin"))
+  api(project(":sdk-serde-kotlinx"))
   api(project(":sdk-spring-boot"))
 
+  // Spring boot starter for kotlin dependencies brought in here for convenience
+  api(libs.spring.boot.starter)
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+  // Testing
   kspTest(project(":sdk-api-kotlin-gen"))
   testImplementation(project(":sdk-testing"))
   testImplementation(libs.kotlinx.coroutines.test)
   testImplementation(libs.spring.boot.starter.test)
-
-  // We need these for the deployment manifest
   testImplementation(project(":sdk-core"))
   testImplementation(libs.jackson.annotations)
   testImplementation(libs.jackson.databind)
--- sdk-spring-boot-starter/build.gradle.kts
+++ sdk-spring-boot-starter/build.gradle.kts
@@ -10,36 +10,32 @@ description = "Restate SDK Spring Boot starter"
 dependencies {
   compileOnly(libs.jspecify)
 
-  api(project(":sdk-spring-boot"))
-  api(project(":sdk-api")) {
-    // Let spring bring jackson in
-    exclude(group = "com.fasterxml.jackson")
-    exclude(group = "com.fasterxml.jackson.core")
-    exclude(group = "com.fasterxml.jackson.datatype")
-  }
-  api(project(":client")) {
-    // Let spring bring jackson in
-    exclude(group = "com.fasterxml.jackson")
-    exclude(group = "com.fasterxml.jackson.core")
-    exclude(group = "com.fasterxml.jackson.datatype")
-  }
-  api(project(":sdk-serde-jackson")) {
-    // Let spring bring jackson in
-    exclude(group = "com.fasterxml.jackson")
-    exclude(group = "com.fasterxml.jackson.core")
-    exclude(group = "com.fasterxml.jackson.datatype")
-  }
+  val excludeJackson =
+      fun ProjectDependency.() {
+        // Let spring bring jackson in
+        exclude(group = "com.fasterxml.jackson")
+        exclude(group = "com.fasterxml.jackson.core")
+        exclude(group = "com.fasterxml.jackson.datatype")
+      }
 
+  // SDK dependencies
+  api(project(":sdk-spring-boot"))
+  api(project(":sdk-api"), excludeJackson)
+  api(project(":client"), excludeJackson)
+  api(project(":sdk-serde-jackson"), excludeJackson)
+
+  // Spring boot starter brought in here for convenience
+  api(libs.spring.boot.starter)
+
+  // Testing
+  testImplementation(libs.spring.boot.starter.test)
+  testAnnotationProcessor(project(":sdk-api-gen"))
   // We need these for the deployment manifest
   testImplementation(project(":sdk-core"))
   testImplementation(libs.jackson.annotations)
   testImplementation(libs.jackson.databind)
-
-  testAnnotationProcessor(project(":sdk-api-gen"))
   testImplementation(project(":sdk-serde-jackson"))
   testImplementation(project(":sdk-testing"))
-
-  testImplementation(libs.spring.boot.starter.test)
 }
 
 tasks.withType<JavaCompile> { options.compilerArgs.add("-parameters") }
