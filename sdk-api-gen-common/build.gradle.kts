096de5d04c52670d18fa9585cf4ad5404a8dcefd
Jens Teglhus Møller <jetm@danskespil.dk>
2025-11-01 08:32:53 +0100

    bump handlebars dependency from 4.3.1 to 4.5.0
    Resolves a build warning due to incompatible versions of slf4j.
    
    Change in HandlebarsTemplateEngine is due to a change in handlebars where they no longer shade commons text in their jar.
    
    Exclude the nashhorn-core dependency from handlebars since it is not license compliant. It is only needed for javascript support in templates which we do not use.

--- sdk-api-gen-common/build.gradle.kts
+++ sdk-api-gen-common/build.gradle.kts
@@ -9,7 +9,11 @@ description = "Restate SDK API Gen Common"
 dependencies {
   compileOnly(libs.jspecify)
 
-  api(libs.handlebars)
+  api(libs.handlebars) {
+    // exclude nashorn-core, since it is not license compliant and is only
+    // needed for javascript in templates which we don't use.
+    exclude(group = "org.openjdk.nashorn", module = "nashorn-core")
+  }
   api(project(":sdk-common"))
 
   // We need it to silence the slf4j warning (coming from handlebars)
