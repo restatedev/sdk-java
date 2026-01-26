64de0b0094771606d627635043514e9188371938
slinkydeveloper <me@slinkydeveloper.com>
2026-01-26 17:09:41 +0100

    Bump spotless

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,5 +11,5 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.10")
     implementation("org.jetbrains.kotlin:kotlin-serialization:2.2.10")
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:7.2.1")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:8.2.0")
 }
