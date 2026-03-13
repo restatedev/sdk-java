fad4fbfefcde274c4e78272a2287279fc4f37c80
slinkydeveloper <me@slinkydeveloper.com>
2026-03-13 10:35:06 +0100

    Add TerminalException.metadata field

--- sdk-common/src/main/java/dev/restate/sdk/common/TerminalException.java
+++ sdk-common/src/main/java/dev/restate/sdk/common/TerminalException.java
@@ -8,6 +8,9 @@
 // https://github.com/restatedev/sdk-java/blob/main/LICENSE
 package dev.restate.sdk.common;
 
+import java.util.Map;
+import java.util.Objects;
+
 /** When thrown in a Restate service method, it will complete the invocation with an error. */
 public class TerminalException extends RuntimeException {
 
@@ -17,6 +20,7 @@ public class TerminalException extends RuntimeException {
   public static final int INTERNAL_SERVER_ERROR_CODE = 500;
 
   private final int code;
+  private final Map<String, String> metadata;
 
   public TerminalException() {
     this(INTERNAL_SERVER_ERROR_CODE);
@@ -34,16 +38,37 @@ public class TerminalException extends RuntimeException {
    * @param message error message
    */
   public TerminalException(int code, String message) {
-    super(message);
-    this.code = code;
+    this(code, message, null);
   }
 
   /**
    * Like {@link #TerminalException(int, String)}, with code {@link #INTERNAL_SERVER_ERROR_CODE}.
    */
   public TerminalException(String message) {
+    this(INTERNAL_SERVER_ERROR_CODE, message, null);
+  }
+
+  /**
+   * Create a new {@link TerminalException}.
+   *
+   * @param code HTTP response status code
+   * @param message error message
+   * @param metadata error metadata (supported only from Restate > 1.6)
+   */
+  public TerminalException(int code, String message, Map<String, String> metadata) {
     super(message);
-    this.code = INTERNAL_SERVER_ERROR_CODE;
+    this.code = code;
+    this.metadata = Objects.requireNonNullElse(metadata, Map.of());
+  }
+
+  /**
+   * Create a new {@link TerminalException}.
+   *
+   * @param message error message
+   * @param metadata error metadata (supported only from Restate > 1.6)
+   */
+  public TerminalException(String message, Map<String, String> metadata) {
+    this(INTERNAL_SERVER_ERROR_CODE, message, metadata);
   }
 
   /**
@@ -51,5 +76,12 @@ public class TerminalException extends RuntimeException {
    */
   public int getCode() {
     return code;
+  }
+
+  /**
+   * @return the error metadata
+   */
+  public Map<String, String> getMetadata() {
+    return metadata;
   }
 }
